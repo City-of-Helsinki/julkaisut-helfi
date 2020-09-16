229e218d3d6e042412f632392e4c729a893295f7
Oskar Schöldström <m@oxy.fi>
2020-09-16 16:46:20 -0300

    fix(gutenberg): fix image block becoming invalid on post edit

--- web/themes/custom/julkaisut/assets/scripts/editor.js
+++ web/themes/custom/julkaisut/assets/scripts/editor.js
@@ -3,6 +3,8 @@ import {
   unregisterBlockStyle,
   registerBlockStyle,
 } from '@wordpress/blocks';
+import { createHigherOrderComponent} from '@wordpress/compose';
+import { addFilter } from '@wordpress/hooks';
 
 domReady(() => {
   registerBlockStyle('core/image', {
@@ -10,3 +12,20 @@ domReady(() => {
     label: 'Basic Koros Top',
   });
 });
+
+addFilter(
+  'editor.BlockListBlock',
+  'julkaisut/fix-drupal-media-attributes',
+  createHigherOrderComponent((BlockListBlock) => {
+    return (props) => {
+      if (props.name === 'core/image') {
+        props.attributes.mediaAttrs = {};
+
+        if (typeof props.attributes.caption === 'object') {
+          props.attributes.caption = props.attributes.caption.rendered;
+        }
+      }
+      return <BlockListBlock { ...props } />;
+    }
+  })
+);
