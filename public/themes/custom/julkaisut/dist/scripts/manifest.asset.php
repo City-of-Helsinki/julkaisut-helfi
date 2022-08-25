7f4e0fe052ba23ec7188ad3f59cefcaf2afc16eb
Arsene Claudiu Ion <arseneclaudiuion@sig-9-145-35-55.uk.ibm.com>
2022-08-25 11:32:38 +0300

    added compiled js files

--- public/themes/custom/julkaisut/dist/scripts/manifest.asset.php
+++ public/themes/custom/julkaisut/dist/scripts/manifest.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-hooks'), 'version' => 'ce98ab8081aa291e6369934a3726e1cf');
+<?php return array('dependencies' => array('wp-blocks', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-hooks'), 'version' => '228b1629d371713dc10a0c72a1fcb520');
